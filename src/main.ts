623322bd7c0f03a6e1ff135eb0ef47b919753bd4
MarcHamamji <hamamjimarc@gmail.com>
2022-11-07 17:48:19 +0200

    Add Vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
+import { inject } from '@vercel/analytics';
 
 import App from './App.vue'
 import router from './router'
@@ -17,3 +18,5 @@ app.use(router)
 app.use(ElementPlus)
 
 app.mount('#app')
+
+inject();
